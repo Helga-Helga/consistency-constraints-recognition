88720bc73e784bf35d6fcc5b39b863168fc9cb0e
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-10-13 16:57:53 +0300

    Rename sum_of_labels to sums because it is a matrix of sums

--- lab1_image_denoising/src/gibbs_sampling.py
+++ lab1_image_denoising/src/gibbs_sampling.py
@@ -20,12 +20,12 @@ from image_generation import (
 )
 
 
-def get_labeling(sum_of_zero_labels, sum_of_unit_labels):
-    height, width = sum_of_zero_labels.shape
+def get_labeling(sums_of_zero_labels, sums_of_unit_labels):
+    height, width = sums_of_zero_labels.shape
     labeling = zeros(shape=(height, width))
     for i in range(height):
         for j in range(width):
-            if sum_of_zero_labels[i][j] > sum_of_unit_labels[i][j]:
+            if sums_of_zero_labels[i][j] > sums_of_unit_labels[i][j]:
                 labeling[i][j] = 0
             else:
                 labeling[i][j] = 1
@@ -35,37 +35,13 @@ def get_labeling(sum_of_zero_labels, sum_of_unit_labels):
 def gibbs_sampling(initial_image, noised_image, epsilon, beta, iterations):
     print("Image denoising with Gibbs sampler...")
     height, width = initial_image.shape
-    labeling = random.randint(2, size=(height, width))
-    sum_of_zero_labels = zeros(shape=(height, width))
-    sum_of_unit_labels = zeros(shape=(height, width))
+    labeling = random.randint(2, size=(height, width))  # U{0, 1}
+    sums_of_zero_labels = zeros(shape=(height, width))
+    sums_of_unit_labels = zeros(shape=(height, width))
     for iteration in range(iterations):
-        for i in range(height):
-            for j in range(width):
-                for n in range(4):
-                    sum_zero_edges = 0
-                    sum_unit_edges = 0
-                    if neighbor_exists(height, width, i, j, n):
-                        i_n, j_n = get_neighbor_coordinate(i, j, n)
-                        sum_zero_edges += edge_weight(
-                            0, labeling[i_n][j_n], beta)
-                        sum_unit_edges += edge_weight(
-                            1, labeling[i_n][j_n], beta)
-                zero = exp(-node_weight(
-                    0, noised_image[i][j], epsilon) - sum_zero_edges)
-                unit = exp(-node_weight(
-                    1, noised_image[i][j], epsilon) - sum_zero_edges)
-                t = zero / (zero + unit)
-                x = random.rand()
-                if x < t:
-                    labeling[i][j] = 0
-                else:
-                    labeling[i][j] = 1
-                if iteration > 2000 and iteration % 30 == 0:
-                    if labeling[i][j] == 0:
-                        sum_of_zero_labels += 1
-                    else:
-                        sum_of_unit_labels += 1
-    result = get_labeling(sum_of_zero_labels, sum_of_unit_labels)
+        if iteration > save_after and iteration % save_step == 0:
+            sums_of_zero_labels += labeling ^ 1
+            sums_of_unit_labels += labeling
     imsave('images/labeling.png', labeling, cmap=gray)
     print("Resulting image is saved to \"images/labeling.png\"")
     return labeling
